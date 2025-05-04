f3468c64ed763ea1ae1559deccb7c300e5fa8214
daxter7777 <daxbogomolov@yandex.ru>
2025-05-04 16:20:09 +0700

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -24,8 +24,8 @@ const App = () => {
     <div className='bg-gray-50 min-h-screen'>
       <ToastContainer />
       {token === ""
-        //? <Login setToken={setToken} />
-        //: <>
+        ? <Login setToken={setToken} />
+        : <>
           <Navbar setToken={setToken} />
           <hr />
           <div className='flex w-full'>
